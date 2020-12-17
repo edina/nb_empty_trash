f4f9a9e34711c4c992163511212093bf5aa19bb2
Ian Stuart <ian.stuart@ed.ac.uk>
2020-12-17 09:06:36 +0000

    change trash button tooltip

--- empty_trash/static/trash.js
+++ empty_trash/static/trash.js
@@ -90,7 +90,7 @@ define([
 
         if ( $('#btnDeleteTrash').length ) {
             $('#btnDeleteTrash').attr(
-                'title', 'Trash', // size: ' + trashUsage
+                'title', 'Empty Trash', // size: (' + trashUsage + ')'
             )
         } else {
             $('#trash-disk-metric').append(
